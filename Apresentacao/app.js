5025ac70bc92c19b7c81b9b285ee6b1a5d96edf2
Maria Heloisa <mariaheloisa@pontogovsistemas.com.br>
2025-08-05 16:32:23 -0300

    feat: Adicionado troca de tema

--- Apresentacao/app.js
+++ Apresentacao/app.js
@@ -4,11 +4,28 @@ botaoCurriculo.addEventListener("click", onClickCurriculo)
 let textoDescricao = document.getElementById("descricao")
 let nomeVisitante = document.getElementById("campoNome")
 
+let botaoTema = document.getElementById("Tema")
+botaoTema.addEventListener("click", mudarTema)
+
+let body = document.getElementById("body")
+
+
 function onClickCurriculo(){
 
 let valorCampoNome = nomeVisitante.value    
 textoDescricao.innerHTML = valorCampoNome + ", Seguem as informações complementares!"
 nomeVisitante.remove(campoNome);
+}
+
+function mudarTema(){
+    if(body.classList.contains("dark")){
+        body.classList.remove("dark"); 
+    }
+    else{
+        body.classList.add("dark")
+    }
+
+
 }
 
 
