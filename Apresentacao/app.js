cb927fdc491550e0ae887f5d96714035d1839d75
Maria Heloisa <mariaheloisa@pontogovsistemas.com.br>
2025-08-05 14:20:17 -0300

    feat: Adicionado remoção campoNome

--- Apresentacao/app.js
+++ Apresentacao/app.js
@@ -1,11 +1,16 @@
 let botaoCurriculo = document.getElementById("curriculo")
 botaoCurriculo.addEventListener("click", onClickCurriculo)
 
-let textoDescricao = document.getElementById("p2")
+let textoDescricao = document.getElementById("descricao")
+let nomeVisitante = document.getElementById("campoNome")
 
 function onClickCurriculo(){
-textoDescricao.innerHTML = "Novo Textos"
+
+let valorCampoNome = nomeVisitante.value    
+textoDescricao.innerHTML = valorCampoNome + ", Seguem as informações complementares!"
+nomeVisitante.remove(campoNome);
 }
+
 
 console.log("mensagem")
 
