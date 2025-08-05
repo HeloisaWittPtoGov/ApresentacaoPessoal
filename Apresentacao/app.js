e201607e996259eef2df5bf2c9ee1e8cc5ae65b5
Maria Heloisa <mariaheloisa@pontogovsistemas.com.br>
2025-08-05 13:28:27 -0300

    feat: Adiconado alteração de texto botão

---
+++ Apresentacao/app.js
@@ -0,0 +1,14 @@
+let botaoCurriculo = document.getElementById("curriculo")
+botaoCurriculo.addEventListener("click", onClickCurriculo)
+
+let textoDescricao = document.getElementById("p2")
+
+function onClickCurriculo(){
+textoDescricao.innerHTML = "Novo Textos"
+}
+
+console.log("mensagem")
+
+
+
+
