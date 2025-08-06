7446957113e061af1e034950885d5c93d8a17b8d
Maria Heloisa <mariaheloisa@pontogovsistemas.com.br>
2025-08-06 09:42:31 -0300

    feat: Adicionado lista de hobbies

--- Apresentacao/app.js
+++ Apresentacao/app.js
@@ -9,11 +9,18 @@ botaoTema.addEventListener("click", mudarTema)
 
 let body = document.getElementById("body")
 
+let botaoGravar = document.getElementById("gravar")
+botaoGravar.addEventListener("click", AddeExibirHobbies)
+
+let campoHobbies = document.getElementById("campoHobbies")
+let exibirHobbies = document.getElementById("exibiListaHobbies")
+let arrayHobbies = []
+
 
 function onClickCurriculo(){
 
 let valorCampoNome = nomeVisitante.value    
-textoDescricao.innerHTML = valorCampoNome + ", Seguem as informações complementares!"
+textoDescricao.innerHTML = valorCampoNome + ", Seguem as informações complementares!" 
 nomeVisitante.remove(campoNome);
 }
 
@@ -24,10 +31,22 @@ function mudarTema(){
     else{
         body.classList.add("dark")
     }
-
-
 }
 
+function AddeExibirHobbies(){
+    arrayHobbies.push(campoHobbies.value)
+    campoHobbies.value = ""
+    atualizaHobbies()
+}
+
+function atualizaHobbies(){
+    exibirHobbies.innerHTML = ""
+    for( let i = 0; i < arrayHobbies.length;i ++){
+        const item = document.createElement("li")
+        item.textContent = arrayHobbies[i]
+        exibirHobbies.appendChild(item)
+    }
+}
 
 console.log("mensagem")
 
